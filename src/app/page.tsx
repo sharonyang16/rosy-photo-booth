5fab93ad383c7688c72a3c32a00044060ac3cb7a
Sharon Yang <sharonyyang0@gmail.com>
2025-04-12 17:17:33 -0400

    Remove eslint disable comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 import { usePhotobooth } from "@/hooks/usePhotobooth";
 import CapturePage from "@/pages/CapturePage/CapturePage";
