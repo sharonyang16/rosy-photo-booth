b995886a0dee49519370d1382dda53ab54ef9fe2
Sharon Yang <sharonyyang0@gmail.com>
2025-04-12 19:34:18 -0400

    Add filters and change package for downloadfunctionality

--- src/hooks/useCustomize.ts
+++ src/hooks/useCustomize.ts
@@ -1,32 +1,31 @@
 import { useState, useRef } from "react";
-import html2canvas from "html2canvas-pro";
+import { toPng } from "html-to-image";
 
 export const useCustomize = () => {
   const [backgroundColor, setBackgroundColor] = useState("sky");
+  const [filter, setFilter] = useState("none");
   const downloadableImageRef = useRef<HTMLDivElement>({} as HTMLDivElement);
 
-  const handleDownloadImage = async () => {
-    const element = downloadableImageRef.current;
-    const canvas = await html2canvas(element);
-
-    const data = canvas.toDataURL("image/jpg");
-    const link = document.createElement("a");
-
-    if (typeof link.download === "string") {
-      link.href = data;
-      link.download = "image.jpg";
-
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-    } else {
-      window.open(data);
-    }
+  const handleDownloadImage = () => {
+    toPng(downloadableImageRef.current, { cacheBust: false, skipFonts: true })
+      .then((dataUrl) => {
+        const link = document.createElement("a");
+        link.download = `rosy-photo-booth-${new Date().toLocaleDateString(
+          "en-US"
+        )}.png`;
+        link.href = dataUrl;
+        link.click();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   return {
     backgroundColor,
     setBackgroundColor,
+    filter,
+    setFilter,
     downloadableImageRef,
     handleDownloadImage,
   };
--- src/types/constants.ts
+++ src/types/constants.ts
@@ -1,3 +1,17 @@
 export const PICTURE_COUNT = 4;
 
 export const BACKGROUND_COLORS = ["sky", "yellow", "pink"];
+
+export const backgroundColorToClass = new Map<string, string>([
+  ["sky", "bg-sky-500"],
+  ["yellow", "bg-yellow-500"],
+  ["pink", "bg-pink-500"],
+]);
+
+export const FILTERS = ["none", "grayscale", "sepia"];
+
+export const filterToClass = new Map<string, string>([
+  ["none", ""],
+  ["grayscale", "grayscale"],
+  ["sepia", "sepia"],
+]);
